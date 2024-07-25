c36aa4799327eadf4242b03c56771fda27d3e5e2
necobra <necobrane105@gmail.com>
2024-07-25 21:21:56 +0300

    solution

---
+++ cinema/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from cinema.views import (
+    movie_list,
+    movie_detail
+)
+
+
+app_name = "cinema"
+
+urlpatterns = [
+    path("movies/",
+         movie_list,
+         name="movie-list"),
+    path("movie/<int:pk>",
+         movie_detail,
+         name="movie-detail"),
+]
